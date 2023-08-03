a7a7fde89db68f29bb126ab3e7e84ca3d04461e1
ansonruby <amaander011057@gmail.com>
2023-08-03 11:58:56 -0500

    mejorar en Mod_Speaker_Lud

---
+++ WebSockets/app/Mod_Speaker.py
@@ -0,0 +1,38 @@
+from lib.Lib_Settings import Get_Mod_Speaker
+from lib.Lib_WS import WebSocketFuseaccess
+from gtts import gTTS
+import subprocess
+import time
+import os
+
+CONFIG_SPEAKER = Get_Mod_Speaker()
+if not "Tiempo_Reset_WS" in CONFIG_SPEAKER:
+    CONFIG_SPEAKER["Tiempo_Reset_WS"] = "120"
+print(CONFIG_SPEAKER["Tiempo_Reset_WS"])
+
+CURRENT_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+
+
+class WSSpeaker(WebSocketFuseaccess):
+    def on_message(self, msg):
+        mytext = msg['message']
+        language = 'es'
+        tld = 'com.mx'
+        myobj = gTTS(text=mytext, lang=language, tld=tld, slow=False)
+        tmp_path = CURRENT_DIR_PATH+"/tmp"
+        if not os.path.exists(tmp_path):
+            os.makedirs(tmp_path)
+        file_name = tmp_path+"/voz_"+str(int(time.time()*1000))+".mp3"
+        myobj.save(file_name)
+        subprocess.call(["cvlc", "--play-and-exit", file_name],
+                        stdout=open(os.devnull, "w"), stderr=subprocess.STDOUT)
+        os.remove(file_name)
+
+
+ws = WSSpeaker()
+
+while True:
+    print("otra vez")
+    ws.create_connection()
+    time.sleep(int(CONFIG_SPEAKER["Tiempo_Reset_WS"]))
+    ws.close_connection()
---
+++ WebSockets/app/lib/Lib_WS.py
@@ -0,0 +1,44 @@
+from actioncable.connection import Connection
+from actioncable.subscription import Subscription
+from actioncable.message import Message
+from lib.Lib_Settings import Get_Rout_server
+from lib.Fun_Dispositivo import Get_ID_Dispositivo
+
+
+class WebSocketFuseaccess():
+
+    def __init__(self):
+        url = Get_Rout_server()
+        ws_url = url.replace("http", "ws", 1) + "/cable"
+        self.ws_url = ws_url
+        self.uuid = Get_ID_Dispositivo()
+
+    def create_connection(self):
+        connection = Connection(url=self.ws_url)
+        connection.connect()
+
+        subscription = Subscription(connection,
+                                    identifier={
+                                        'channel': 'SpeakerNotificationsChannel', 'uuid': self.uuid}
+                                    )
+
+        subscription.on_receive(callback=self.on_message)
+        subscription.create()
+        self.connection = connection
+        self.subscription = subscription
+
+    def close_connection(self):
+        if self.connection and self.subscription:
+            self.subscription.remove()
+            self.connection.disconnect()
+            self.subscription = None
+            self.connection = None
+
+    def send_message(self, action, data):
+        if self.connection and self.subscription:
+            message = Message(action=action, data=data)
+            self.subscription.send(message)
+            return True
+
+    def on_message(self, msg):
+        pass
