51989b901da8802d9338dbcfda9b75a216197ae8
“Lud <ludingnumpaque@gmail.com>
2024-02-27 12:59:22 -0500

    Added threading

--- WebSockets/app/Mod_Speaker.py
+++ WebSockets/app/Mod_Speaker.py
@@ -1,9 +1,11 @@
 from lib.Lib_Settings import Get_Mod_Speaker
 from lib.Lib_WS import WebSocketFuseaccess
 from gtts import gTTS
+from threading import Thread
 import subprocess
 import time
 import os
+import time
 
 CONFIG_SPEAKER = Get_Mod_Speaker()
 if not "Tiempo_Reset_WS" in CONFIG_SPEAKER:
@@ -12,20 +14,26 @@ if not "Tiempo_Reset_WS" in CONFIG_SPEAKER:
 CURRENT_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
 
+def speak(msg):
+    mytext = msg['message']
+    language = 'es'
+    tld = 'com.mx'
+    myobj = gTTS(text=mytext, lang=language, tld=tld, slow=False)
+    tmp_path = CURRENT_DIR_PATH + "/tmp"
+    if not os.path.exists(tmp_path):
+        os.makedirs(tmp_path)
+    file_name = tmp_path + "/voz_" + str(int(time.time() * 1000)) + ".mp3"
+    myobj.save(file_name)
+    subprocess.call(["cvlc", "--play-and-exit", file_name],
+                    stdout=open(os.devnull, "w"), stderr=subprocess.STDOUT)
+    os.remove(file_name)
+
+
 class WSSpeaker(WebSocketFuseaccess):
     def on_message(self, msg):
-        mytext = msg['message']
-        language = 'es'
-        tld = 'com.mx'
-        myobj = gTTS(text=mytext, lang=language, tld=tld, slow=False)
-        tmp_path = CURRENT_DIR_PATH + "/tmp"
-        if not os.path.exists(tmp_path):
-            os.makedirs(tmp_path)
-        file_name = tmp_path + "/voz_" + str(int(time.time() * 1000)) + ".mp3"
-        myobj.save(file_name)
-        subprocess.call(["cvlc", "--play-and-exit", file_name],
-                        stdout=open(os.devnull, "w"), stderr=subprocess.STDOUT)
-        os.remove(file_name)
+        T = Thread(target=speak, args=(msg,))
+        T.setDaemon(True)
+        T.start()
 
 
 ws = WSSpeaker()
