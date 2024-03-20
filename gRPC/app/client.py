8b8be2bcbc09d0973e55307fb9773cc02823f2d0
“Lud <ludingnumpaque@gmail.com>
2024-03-20 14:34:20 -0500

    Undo tries of optimice

--- gRPC/app/client.py
+++ gRPC/app/client.py
@@ -9,7 +9,7 @@ import sirena_pb2
 import sirena_pb2_grpc
 
 
-async def get_grpc_stream():
+async def run():
     uuid = Get_ID_Dispositivo()
     channel_options = [
         ("grpc.keepalive_time_ms", 60000),
@@ -17,34 +17,29 @@ async def get_grpc_stream():
         ("grpc.keepalive_permit_without_calls", 1),
         ("grpc.http2.max_pings_without_data", 5),
     ]
-    async with grpc.aio.secure_channel(
-        "communication.fusepong.com",
-        grpc.ssl_channel_credentials(),
-        options=channel_options
-    ) as channel:
-
-        try:
-            stub = sirena_pb2_grpc.SirenaServiceStub(channel)
-            stream = stub.SendEvents(sirena_pb2.Channel(uuid=uuid))
-            while True:
-                response = await stream.read()
-                if response == grpc.aio.EOF:
-                    break
-                send_speaker_msg(
-                    {
-                        "priority": response.priority,
-                        "message": response.message,
-                        "audio": response.audio
-                    }
-                )
-
-        except Exception as e:
-            print(e)
-
-
-async def main():
     while True:
-        await get_grpc_stream()
+        async with grpc.aio.secure_channel(
+            "communication.fusepong.com",
+            grpc.ssl_channel_credentials(),
+            options=channel_options
+        ) as channel:
+
+            while True:
+                time.sleep(0.1)
+                asyncio.sleep(0.1)
+                try:
+                    stub = sirena_pb2_grpc.SirenaServiceStub(channel)
+                    async for response in stub.SendEvents(sirena_pb2.Channel(uuid=uuid)):
+                        send_speaker_msg(
+                            {
+                                "priority": response.priority,
+                                "message": response.message,
+                                "audio": response.audio
+                            }
+                        )
+                except Exception as e:
+                    print(e)
+
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(run())
