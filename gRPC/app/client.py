2fdb24d5469047a167b4999bb2c1c7367152c5e2
“Lud <ludingnumpaque@gmail.com>
2024-03-20 13:06:00 -0500

    Added logic for retris

--- gRPC/app/client.py
+++ gRPC/app/client.py
@@ -17,34 +17,29 @@ async def run():
         ("grpc.keepalive_permit_without_calls", 1),
         ("grpc.http2.max_pings_without_data", 5),
     ]
-    try:
-        async with grpc.aio.secure_channel(
-            "communication.fusepong.com",
-            grpc.ssl_channel_credentials(),
-            options=channel_options
-        ) as channel:
+    async with grpc.aio.secure_channel(
+        "communication.fusepong.com",
+        grpc.ssl_channel_credentials(),
+        options=channel_options
+    ) as channel:
 
-            while True:
-                time.sleep(0.1)
-                asyncio.sleep(0.1)
-                try:
-                    stub = sirena_pb2_grpc.SirenaServiceStub(channel)
-                    async for response in stub.SendEvents(sirena_pb2.Channel(uuid=uuid)):
-                        send_speaker_msg(
-                            {
-                                "priority": response.priority,
-                                "message": response.message,
-                                "audio": response.audio
-                            }
-                        )
-                except Exception as e:
-                    print(e)
-
-    except Exception as e:
-        print(e)
-    finally:
-        asyncio.run(run())
+        while True:
+            time.sleep(0.1)
+            asyncio.sleep(0.1)
+            try:
+                stub = sirena_pb2_grpc.SirenaServiceStub(channel)
+                async for response in stub.SendEvents(sirena_pb2.Channel(uuid=uuid)):
+                    send_speaker_msg(
+                        {
+                            "priority": response.priority,
+                            "message": response.message,
+                            "audio": response.audio
+                        }
+                    )
+            except Exception as e:
+                print(e)
 
 
 if __name__ == "__main__":
-    asyncio.run(run())
+    while True:
+        asyncio.run(run())
